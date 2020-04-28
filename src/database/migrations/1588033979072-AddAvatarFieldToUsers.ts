a838c0800f2ecc3bdbe4781695fbd00f71c70548
Vinicius Lima <paulo.vinicius.f.l@live.com>
2020-04-27 21:50:32 -0300

    add new migrate for new fild for avatar in users

--- src/database/migrations/1588033979072-AddAvatarFieldToUsers.ts
+++ src/database/migrations/1588033979072-AddAvatarFieldToUsers.ts
@@ -1,11 +1,18 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
-export class AddAvatarFieldToUsers1588033979072 implements MigrationInterface {
+export default class AddAvatarFieldToUsers1588033979072 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.addColumn('users', new TableColumn({
+            name: 'avatar',
+            type: 'varchar',
+            isNullable: true,
+        }),
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.dropColumn('users', 'avatar')
     }
 
 }
