60ddb970b825c8a0588e8b13843918c3e8fe5da1
Vinicius Lima <paulo.vinicius.f.l@live.com>
2020-04-28 20:50:15 -0300

    new configs for uploads in files for app and show files in img

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,6 +4,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
 import ensureAutehnticaded from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -32,8 +33,19 @@ usersRouter.post('/', async (request, response) => {
 });
 
 usersRouter.patch('/avatar', ensureAutehnticaded, upload.single('avatar'), async (request, response) =>{
-  return response.json({ok: true});
+  try {
+    const updateUserAvatar = new UpdateUserAvatarService();
 
+   const user = await  updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    delete user.password;
+    return response.json(user)
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 export default usersRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import routes from './routes';
+import uploadConfigs from './config/upload';
 import 'reflect-metadata'
 import './database';
 
 const app = express();
 app.use(express.json());
+app.use('/files', express.static(uploadConfigs.directory));
 app.use(routes);
 
 app.listen(3333, () => {
