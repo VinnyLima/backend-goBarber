ff7461d241076646c1738d17e72d1939f960d9ea
Vinicius Lima <paulo.vinicius.f.l@live.com>
2020-07-29 13:25:46 -0300

    realizando restruturação

---
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -0,0 +1,8 @@
+import Appointment from '../infra/typeorm/entities/Appointments';
+
+export default interface IAppointmentsRepository {
+
+    findByDate(date: Date) : Promise<Appointment | undefined>
+
+
+}
